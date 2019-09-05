b071bafaa0c50e40248e611435a7cb497d2be1c0
Kash Nouroozi <knouroozi@gmail.com>
2019-09-04 21:38:11 -0400

    Let's go

--- iterm-workspace.rb
+++ iterm-workspace.rb
@@ -2,12 +2,11 @@ class ItermWorkspace < Formula
   desc "Configuration-based iTerm scripting for starting up your project workspace."
   homepage "https://github.com/knrz/iterm-workspace"
   url "https://github.com/knrz/iterm-workspace/archive/1.0.0.tar.gz"
-  sha256 "f9b8dd4de9506c9b59d01a48b6cd7e02bfa20be34c24c0047ad3850032faf416"
+  sha256 "f463f877793e0abc6ba5b7faa5106d3b1bc04736b3a7c17ca64af295e5441573"
 
   bottle :unneeded
 
   def install
-    bin.install "init-iterm-workspace" 
     bin.install "start-iterm-workspace" 
     bin.install "bootstrap-iterm-workspace" 
   end
