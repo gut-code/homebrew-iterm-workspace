2e3cab2ed8e7f78701532c2a9c8f337a93d6b51c
Kash Nouroozi <knouroozi@gmail.com>
2019-11-14 16:21:27 -0500

    :)

--- iterm-workspace.rb
+++ iterm-workspace.rb
@@ -1,8 +1,8 @@
 class ItermWorkspace < Formula
   desc "Configuration-based iTerm scripting for starting up your project workspace."
   homepage "https://github.com/knrz/iterm-workspace"
-  url "https://github.com/knrz/iterm-workspace/archive/1.2.0.tar.gz"
-  sha256 "ca855f6417f519aef8adaa3bab14ab5bb7ddf1577c2486b654b1e607c54440be"
+  url "https://github.com/knrz/iterm-workspace/archive/1.2.1.tar.gz"
+  sha256 "da9bbda35f46824a9de2a0f524f50c9a497f32731c539736f3002f9f1e99a7a5"
 
   bottle :unneeded
 
