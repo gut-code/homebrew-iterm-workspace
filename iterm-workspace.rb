9c21578a89256c8403744b341771325057686cc4
gut-code <51887125+gut-code@users.noreply.github.com>
2022-01-06 17:44:40 +0100

    Remove deprecated option `bottle :unneeded`
    - [How to handle bottle :unneeded deprecation for external taps](https://github.com/Homebrew/discussions/discussions/2311)
    - [Bottle all bottle :unneeded](https://github.com/Homebrew/homebrew-core/issues/75943)
    - [formula_auditor: don't allow bottle :unneeded in core](https://github.com/Homebrew/brew/pull/11239)

--- iterm-workspace.rb
+++ iterm-workspace.rb
@@ -4,8 +4,6 @@ class ItermWorkspace < Formula
   url "https://github.com/gut-code/iterm-workspace/archive/1.2.2.tar.gz"
   sha256 "1446ae087c9ddb4ca9b0abb5ec10884f1946359c0ec72f0ce143a2bad2750cd1"
 
-  bottle :unneeded
-
   def install
     bin.install "start-iterm-workspace"
     bin.install "bootstrap-iterm-workspace"
